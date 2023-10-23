a5f1b062728cf45287ac6a754101c9b488f6bb56
Nick_Robinson <Nick.Robinson-c@mnx.com>
2023-10-23 17:05:37 -0400

    DB configuring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
--- insert into roles (name) values ('ROLE_USER');
--- insert into roles (name) values ('ROLE_MODERATOR');
--- insert into roles (name) values ('ROLE_ADMIN');
+
+
+insert into ROLES (name) values ('ROLE_USER');
+insert into ROLES (name) values ('ROLE_MODERATOR');
+insert into ROLES (name) values ('ROLE_ADMIN');
 --
 -- INSERT INTO users (username, email, password, role) VALUES ('nick964', 'nickr964@gmail.com', 'mytestpass', 'ROLE_USER')
 --
@@ -11,7 +13,7 @@
 -- INSERT INTO question_option (option, input_type) VALUES ('O,U', 'radio')
 -- INSERT INTO question_option (option, input_type) VALUES ('Y,N', 'radio')
 --
- INSERT INTO questions (id, text, section, question_type, line_value) VALUES (1, '1st Quarter', 'Game Points', 1, 9.5)
+ --INSERT INTO questions (id, text, section, question_type, line_value) VALUES (1, '1st Quarter', 'Game Points', 1, 9.5)
 -- INSERT INTO questions (text, section, question_type, line_value) VALUES ('2nd Quarter', 'Game Points', 1, 9.5)
 -- INSERT INTO questions (text, section, question_type) VALUES ('First Song "Umbrella"', 'Halftime', 2)
 -- INSERT INTO questions (text, section, question_type) VALUES ('Last Song Diamonds', 'Halftime', 2)
@@ -21,4 +23,4 @@
 -- INSERT INTO question_option_relation (question_id, question_option_id) VALUES (3, 2)
 -- INSERT INTO question_option_relation (question_id, question_option_id) VALUES (4, 2)
 --
- INSERT INTO master_answers (id, question_id, answer) VALUES (1, 1, 'N')
+ --INSERT INTO master_answers (id, question_id, answer) VALUES (1, 1, 'N')
